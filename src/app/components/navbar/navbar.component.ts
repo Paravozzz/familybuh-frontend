501b641f93509c6de228677af267fd471b35bf59
Paravozzz <paravozzz@list.ru>
2023-12-25 00:39:17 +0300

    Перевёл navbar на ng-bootstrap.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, Renderer2, ViewChild} from '@angular/core';
 import {ButtonNameEnum} from "src/app/enums/ButtonNameEnum";
 
 @Component({
@@ -8,5 +8,29 @@ import {ButtonNameEnum} from "src/app/enums/ButtonNameEnum";
 })
 export class NavbarComponent {
   @Output("activeButton") activeButtonEvent = new EventEmitter<ButtonNameEnum>();
+  @ViewChild("navButtons") navButtons!: ElementRef;
   eButtonName = ButtonNameEnum;
+
+  constructor(private render: Renderer2) {
+  }
+
+  navButtonClick($event: MouseEvent, buttonName: ButtonNameEnum) {
+    this.activeButtonEvent.emit(buttonName);
+    this._setActiveLink($event);
+  }
+
+  /**
+   * Сделать ссылку в navbar активной
+   * @param $event
+   * @private
+   */
+  private _setActiveLink($event: MouseEvent) {
+    const children: HTMLCollection = this.navButtons.nativeElement.children;
+    for (let i = 0; i < children.length; i++) {
+      const href: Element | null = children.item(i);
+      this.render.removeClass(href, "active");
+    }
+    const target = $event.target;
+    this.render.addClass(target, "active");
+  }
 }
