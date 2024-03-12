831bd278a2e433686940ea8f6fa2606189c9ed94
Paravozzz <paravozzz@list.ru>
2024-03-12 23:07:07 +0300

    Добавлен routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from "./app.component";
 import {AuthGuard} from "./guards/auth.guard";
+import {IncomesComponent} from "./components/pages/incomes/incomes.component";
+import {ExpensesComponent} from "./components/pages/expenses/expenses.component";
 
 const routes: Routes = [
   {
     path: "",
     pathMatch: "full",
-    // redirectTo: "betonInfo",
-    component: AppComponent,
-    //resolve: {properties: CorePropertiesResolver}
+    component: ExpensesComponent,
     canActivate: [AuthGuard]
-  }
+  },
+  {path: "expenses", component: ExpensesComponent},
+  {path: "incomes", component: IncomesComponent, canActivate: [AuthGuard]},
+  {path: "categories", component: IncomesComponent, canActivate: [AuthGuard]},
+  {path: "accounts", component: IncomesComponent, canActivate: [AuthGuard]},
+  {path: "currencies", component: IncomesComponent, canActivate: [AuthGuard]},
+  {path: "transfers", component: IncomesComponent, canActivate: [AuthGuard]},
+  {path: "exchanges", component: IncomesComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
