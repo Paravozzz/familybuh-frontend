6da1de557c5210f972f290e9a46f0e44db03fe4e
Paravozzz <paravozzz@list.ru>
2024-03-13 20:50:37 +0300

    Исправление ошибок в роутинге.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {AuthGuard} from "./guards/auth.guard";
 import {IncomesComponent} from "./components/pages/incomes/incomes.component";
 import {ExpensesComponent} from "./components/pages/expenses/expenses.component";
+import {CategoriesComponent} from "./components/pages/categories/categories.component";
+import {AccountsComponent} from "./components/pages/accounts/accounts.component";
+import {CurrenciesComponent} from "./components/pages/currencies/currencies.component";
+import {TransfersComponent} from "./components/pages/transfers/transfers.component";
+import {ExchangesComponent} from "./components/pages/exchanges/exchanges.component";
 
 const routes: Routes = [
   {
@@ -13,11 +18,11 @@ const routes: Routes = [
   },
   {path: "expenses", component: ExpensesComponent},
   {path: "incomes", component: IncomesComponent, canActivate: [AuthGuard]},
-  {path: "categories", component: IncomesComponent, canActivate: [AuthGuard]},
-  {path: "accounts", component: IncomesComponent, canActivate: [AuthGuard]},
-  {path: "currencies", component: IncomesComponent, canActivate: [AuthGuard]},
-  {path: "transfers", component: IncomesComponent, canActivate: [AuthGuard]},
-  {path: "exchanges", component: IncomesComponent, canActivate: [AuthGuard]},
+  {path: "categories", component: CategoriesComponent, canActivate: [AuthGuard]},
+  {path: "accounts", component: AccountsComponent, canActivate: [AuthGuard]},
+  {path: "currencies", component: CurrenciesComponent, canActivate: [AuthGuard]},
+  {path: "transfers", component: TransfersComponent, canActivate: [AuthGuard]},
+  {path: "exchanges", component: ExchangesComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
